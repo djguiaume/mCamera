6a16f790ccfcb477abbda208a76a122ffd1c74a2
Emmanuel <darki@localhost.localdomain>
2014-04-16 16:24:34 +0800

    Added MCamera class and appropriate permissions

---
+++ src/com/epitech/mcamera/MCamera.java
@@ -0,0 +1,123 @@
+package com.epitech.mcamera;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore.Files.FileColumns;
+import android.util.Log;
+
+public class MCamera {
+	private static String TAG = "mCamera";
+	private Camera mCamera;
+
+	public MCamera() {
+
+	}
+	
+	public boolean init(Context context) {
+		if (checkCameraHardware(context) == false)
+			return false;
+		mCamera = getCameraInstance();
+		if (mCamera == null)
+			return false;
+		return true;
+	}
+	
+	public void takePicture() {
+		mCamera.takePicture(null, mPicture, mPicture);
+	}
+
+	private boolean checkCameraHardware(Context context) {
+
+		if (context.getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA))
+			return true;
+		else
+			return false;
+	}
+
+	private PictureCallback mPicture = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+
+			File pictureFile = getOutputMediaFile(FileColumns.MEDIA_TYPE_IMAGE);
+			if (pictureFile == null) {
+				Log.d(TAG,
+						"Error creating media file, check storage permissions: "
+								+ e.getMessage());
+				return;
+			}
+
+			try {
+				FileOutputStream fos = new FileOutputStream(pictureFile);
+				fos.write(data);
+				fos.close();
+			} catch (FileNotFoundException e) {
+				Log.d(TAG, "File not found: " + e.getMessage());
+			} catch (IOException e) {
+				Log.d(TAG, "Error accessing file: " + e.getMessage());
+			}
+		}
+	};
+
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	private static File getOutputMediaFile(int type) {
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"MyCameraApp");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+		if (type == FileColumns.MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else if (type == FileColumns.MEDIA_TYPE_VIDEO) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "VID_" + timeStamp + ".mp4");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+}
