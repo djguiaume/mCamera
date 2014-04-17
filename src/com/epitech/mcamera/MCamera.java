d97f8407ccb319aeeaf0317d65a5bd66a13b81fa
Emmanuel <darki@localhost.localdomain>
2014-04-17 20:33:20 +0800

    Add destroy() and threaded takePicture()

--- src/com/epitech/mcamera/MCamera.java
+++ src/com/epitech/mcamera/MCamera.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore.Files.FileColumns;
 import android.util.Log;
@@ -23,7 +24,7 @@ public class MCamera {
 	public MCamera() {
 
 	}
-	
+
 	public boolean init(Context context) {
 		if (checkCameraHardware(context) == false)
 			return false;
@@ -32,13 +33,21 @@ public class MCamera {
 			return false;
 		return true;
 	}
-	
+
+	public void destroy() {
+		mCamera.stopPreview();
+		mCamera.release();
+		mCamera = null;
+	}
+
 	public Camera getCamera() {
 		return mCamera;
 	}
-	
+
 	public void takePicture() {
-		mCamera.takePicture(null, null, mPicture);
+
+		TakePictureTask takePictureTask = new TakePictureTask();
+		takePictureTask.execute();
 	}
 
 	private boolean checkCameraHardware(Context context) {
@@ -64,8 +73,8 @@ public class MCamera {
 
 			try {
 				FileOutputStream fos = new FileOutputStream(pictureFile);
-				Log.d(TAG, "fos="+ fos);
-				Log.d(TAG, "data="+ data);
+				Log.d(TAG, "fos=" + fos);
+				Log.d(TAG, "data=" + data);
 				fos.write(data);
 				fos.close();
 			} catch (FileNotFoundException e) {
@@ -124,5 +133,24 @@ public class MCamera {
 		}
 
 		return mediaFile;
+	}
+
+	private class TakePictureTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected void onPostExecute(Void result) {
+			mCamera.startPreview();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			mCamera.takePicture(null, null, mPicture);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
 	}
 }
