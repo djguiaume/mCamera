8871aac81c78f2f2ac5a72ccca1621c8390fb32f
Emmanuel <darki@localhost.localdomain>
2014-04-18 17:30:44 +0800

    Add video recording, clean code, add Exceptions

--- src/com/epitech/mcamera/MCamera.java
+++ src/com/epitech/mcamera/MCamera.java
@@ -11,24 +11,30 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore.Files.FileColumns;
 import android.util.Log;
+import android.view.SurfaceHolder;
 
 public class MCamera {
-	private static String TAG = "mCamera";
+	private static String TAG = "MCamera";
+	private static String mSaveDir = "MyCameraApp";
 	private Camera mCamera = null;
+	private MediaRecorder mMediaRecorder;
 
 	public MCamera() {
 
 	}
 
 	public boolean init(Context context) {
-		Log.d(TAG, "init mPicture="+mPicture+"mCamera="+mCamera);
-		if (mCamera != null)
+		if (mCamera != null) {
+			Log.d(TAG, "already init.");
 			return false;
+		}
 		if (checkCameraHardware(context) == false)
 			return false;
 		mCamera = getCameraInstance();
@@ -38,27 +44,74 @@ public class MCamera {
 	}
 
 	public void destroy() {
-		Log.d(TAG, "destroy mPicture="+mPicture+"mCamera="+mCamera);
+		Log.d(TAG, "destroy called.");
 		mCamera.stopPreview();
 		mCamera.release();
 		mCamera = null;
 	}
 
+	private void releaseMediaRecorder() {
+		if (mMediaRecorder == null)
+			return;
+		mMediaRecorder.reset();
+		mMediaRecorder.release();
+		mMediaRecorder = null;
+		mCamera.lock();
+	}
+
 	public Camera getCamera() {
-		Log.d(TAG, "getCamera mPicture="+mPicture+"mCamera="+mCamera);
+		if (mCamera == null)
+			Log.w(TAG, "getCamera called without init.");
 		return mCamera;
 	}
 
-	public void takePicture() {
+	public static Camera getCameraInstance() {
 
-		TakePictureTask takePictureTask = new TakePictureTask();
-		takePictureTask.execute();
-		Log.d(TAG, "takePicture mPicture="+mPicture+"mCamera="+mCamera);
-		//mCamera.takePicture(null, null, mPicture);
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+		}
+		return c;
+	}
+
+	private static File getOutputMediaFile(int type) throws Exception {
+
+		if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED)
+			throw new Exception("SDCard not properly mounted.");
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				mSaveDir);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs())
+				throw new Exception("failed to create directory " + mSaveDir);
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+		if (type == FileColumns.MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else if (type == FileColumns.MEDIA_TYPE_VIDEO) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "VID_" + timeStamp + ".mp4");
+		} else {
+			throw new Exception("Unknown media file type");
+		}
+
+		return mediaFile;
 	}
 
 	private boolean checkCameraHardware(Context context) {
-		Log.d(TAG, "CheckCameraHardware mPicture="+mPicture+"mCamera="+mCamera);
+		Log.d(TAG, "CheckCameraHardware mPicture=" + mPicture + "mCamera="
+				+ mCamera);
 		if (context.getPackageManager().hasSystemFeature(
 				PackageManager.FEATURE_CAMERA))
 			return true;
@@ -66,12 +119,24 @@ public class MCamera {
 			return false;
 	}
 
+	public void takePicture() {
+		TakePictureTask takePictureTask = new TakePictureTask();
+		takePictureTask.execute();
+	}
+
 	private PictureCallback mPicture = new PictureCallback() {
 
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
-			Log.d(TAG, "onPictureTaken mPicture="+mPicture+"mCamera="+mCamera);
-			File pictureFile = getOutputMediaFile(FileColumns.MEDIA_TYPE_IMAGE);
+			Log.d(TAG, "onPictureTaken mPicture=" + mPicture + "mCamera="
+					+ mCamera);
+			File pictureFile = null;
+			try {
+				pictureFile = getOutputMediaFile(FileColumns.MEDIA_TYPE_IMAGE);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				Log.e(TAG, e.getMessage());
+			}
 			if (pictureFile == null) {
 				Log.d(TAG,
 						"Error creating media file, check storage permissions.");
@@ -92,58 +157,6 @@ public class MCamera {
 		}
 	};
 
-	public static Camera getCameraInstance() {
-		
-		Camera c = null;
-		try {
-			c = Camera.open(); // attempt to get a Camera instance
-		} catch (Exception e) {
-			Log.e(TAG, e.getMessage());
-		}
-		Log.d(TAG, "getCameraInstance C="+c);
-		return c; // returns nu if camera is unavailable
-	}
-
-	private static Uri getOutputMediaFileUri(int type) {
-		return Uri.fromFile(getOutputMediaFile(type));
-	}
-
-	private static File getOutputMediaFile(int type) {
-		// To be safe, you should check that the SDCard is mounted
-		// using Environment.getExternalStorageState() before doing this.
-
-		File mediaStorageDir = new File(
-				Environment
-						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-				"MyCameraApp");
-		// This location works best if you want the created images to be shared
-		// between applications and persist after your app has been uninstalled.
-
-		// Create the storage directory if it does not exist
-		if (!mediaStorageDir.exists()) {
-			if (!mediaStorageDir.mkdirs()) {
-				Log.d("MyCameraApp", "failed to create directory");
-				return null;
-			}
-		}
-
-		// Create a media file name
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-				.format(new Date());
-		File mediaFile;
-		if (type == FileColumns.MEDIA_TYPE_IMAGE) {
-			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ "IMG_" + timeStamp + ".jpg");
-		} else if (type == FileColumns.MEDIA_TYPE_VIDEO) {
-			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ "VID_" + timeStamp + ".mp4");
-		} else {
-			return null;
-		}
-
-		return mediaFile;
-	}
-
 	private class TakePictureTask extends AsyncTask<Void, Void, Void> {
 
 		@Override
@@ -161,5 +174,63 @@ public class MCamera {
 			}
 			return null;
 		}
+	}
+
+	private boolean prepareVideoRecorder(SurfaceHolder holder) {
+
+		mCamera = getCameraInstance();
+		mMediaRecorder = new MediaRecorder();
+
+		mCamera.unlock();
+		mMediaRecorder.setCamera(mCamera);
+
+		mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+		mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+		mMediaRecorder.setProfile(CamcorderProfile
+				.get(CamcorderProfile.QUALITY_HIGH));
+
+		try {
+			mMediaRecorder.setOutputFile(getOutputMediaFile(
+					FileColumns.MEDIA_TYPE_VIDEO).toString());
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+			return false;
+		}
+
+		// TODO: Do in MySurfaceView?
+		mMediaRecorder.setPreviewDisplay(holder.getSurface());
+
+		try {
+			mMediaRecorder.prepare();
+		} catch (IllegalStateException e) {
+			Log.d(TAG,
+					"IllegalStateException preparing MediaRecorder: "
+							+ e.getMessage());
+			releaseMediaRecorder();
+			return false;
+		} catch (IOException e) {
+			Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+			releaseMediaRecorder();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean startVideoRecording(SurfaceHolder holder) {
+		if (prepareVideoRecorder(holder))
+			mMediaRecorder.start();
+		else {
+			releaseMediaRecorder();
+			return false;
+		}
+		return true;
+
+	}
+
+	public void stoptVideoRecording() {
+		mMediaRecorder.stop();
+		releaseMediaRecorder();
+		mCamera.lock();
 	}
 }
