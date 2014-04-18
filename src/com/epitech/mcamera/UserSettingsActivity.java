3e5e226b66d6ae2f054ed57d7683b13f6cfbf728
Satyre <robin.maitre@gmail.com>
2014-04-18 16:51:25 +0800

    settings conflict

---
+++ src/com/epitech/mcamera/UserSettingsActivity.java
@@ -0,0 +1,34 @@
+package com.epitech.mcamera;
+
+import com.vaya.voicebox.R;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.util.Log;
+ 
+public class UserSettingsActivity extends Activity {
+ 
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	public static class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			Log.d(LOG_TAG, "Create frag settings");
+			addPreferencesFromResource(R.xml.settings);
+		}
+
+    }
+}
